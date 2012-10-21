58d9a87526059ceabc86daa9750c0c7986682e1b
Welington Veiga <welington.veiga@gmail.com>
2012-10-20 23:42:06 -0300

    Criando estrutura incial do projeto.

---
+++ src/main/java/br/com/machinae/assemblae/App.java
@@ -0,0 +1,13 @@
+package br.com.machinae.assemblae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/machinae/assemblae/AppTest.java
@@ -0,0 +1,21 @@
+package br.com.machinae.assemblae;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
